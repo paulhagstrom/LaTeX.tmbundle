47604c5288d2e716ed4a841d4fe768e28be21e0d
Alex Ross <z-github@lasersox.net>
2009-11-13 23:41:13 -0800

    Reworked the Typeset & View command. No functional changes, just some refactoring. Added a “Clean, Typeset & View” command, and deleted the “Trash aux Files” command.

--- Support/bin/bootstrap.sh
+++ Support/bin/bootstrap.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+info() {
+  echo "-->" $*;
+};
+
+seperator(){
+  echo "--------------------------------------------------------------------------------";
+};
+
+perform_command(){
+  if [ -n "$TM_LATEX_DEBUG" ]; then
+    eval echo '$' $* >&2;
+  fi
+  eval $*;
+}
+
 shopt -s extglob;
 
-# get the document's filename.
+# parse the arguments we've been given
+engine=$1;
+if [ $# > 2 ]; then flags="${@:2:$#-1}"; fi
 eval document=\$$#;
 
 # enable synctex by default
@@ -15,13 +32,17 @@ ext=${document##*.}
 basename=$(basename -s ".$ext" "$document");
 
 if [ -n "$TM_LATEX_HIDE_AUX_FILES" ];
-then jobname=".$basename";
+then
+  # this is kind of experimental and doesn't work reliably…
+  # bibtex won't find your aux files and of course it's not
+  # possible to TELL BIBTEX TO LOOK SOMEWHERE ELSE.
+jobname=".$basename";
 else jobname="$basename"; fi
 
+flags="$flags -halt-on-error -no-parse-first-line -file-line-error";
 
-args='-halt-on-error -no-parse-first-line -file-line-error';
-
-if [[ "$1" = *'pdflatex' ]]; then args="$args -output-format=pdf"; fi
+# xelatex will barf on -output-format
+if [[ "$1" = *'pdflatex' ]]; then flags="$flags -output-format=pdf"; fi
 
 # paths to the auxillary files we care about. we will
 # watch changes in these files, and compile accordingly.
@@ -36,48 +57,39 @@ pdf="$jobname.pdf";
 
 # if the document IS a preamble.
 if [[ "$document" = *'.ltx' ]]; then
-  echo "-->Dumping format file";
-  eval echo '$' ${@:1:$#-1} $args -jobname='$jobname' -ini '\"\&latex\"' '\"$document\"' '\\\\dump' >&2;
-  eval ${@:1:$#-1} $args -jobname='$jobname' -ini '\&latex' '"$document"' '\\dump'; rc=$?;
+  
+  info "Dumping format file";
+  
+  perform_command $engine $flags -jobname='$jobname' -ini '\&latex' '"$document"' '\\dump'; rc=$?;
   rm -f "$pdf"; rm -f "$log";
+  
   exit $rc;
 fi
 
-# Set up a basic preamble and wrap the document in \begin{document} / \end{document}
-# when the file is a TeX fragment.
 
+# add a -fmt argument if there is no \documentclass
 if ! grep -q '\\documentclass' "$document"; then
   
   # See if the file specifies a fmt.
   if head -n 1 "$document" | grep -q '^%&'; then
     
-    fmt="$(head -n 1 "$document" | ruby -ne 'puts $_.match(/^%&\s*(\/?(?:\\ |[^ ])*?)(?:\.fmt|\.ltx)?(?=\s)/)[1]')";
+    ltx="$(head -n 1 "$document" | ruby -ne 'puts $_.match(/^%&\s*(\/?(?:\\ |[^ ])*?)(?:\.fmt|\.ltx)?(?=\s)/)[1]')";
     
-  # Otherwise, we use the default.
-  else
-
-    fmt="${TM_LATEX_DEFAULT_FORMAT:-$TM_BUNDLE_SUPPORT/lib/tmdefault.fmt}";
+  else # we use the default.
+    
+    ltx="${TM_LATEX_DEFAULT_FORMAT:-$TM_BUNDLE_SUPPORT/lib/tmdefault.ltx}";
     
   fi
 
-  fmt="${fmt%\.*(fmt|ltx)}.fmt";
-  ltx="${fmt%\.fmt}.ltx";
+  ltx="${ltx%\.*(fmt|ltx)}.ltx";
+  fmt="${ltx%\.ltx}.$engine.fmt";
 
   export TEXFORMATS=":${TEXFORMATS}:$(dirname "$fmt"):"
   
-  args="$args -fmt=\"$(basename -s ".fmt" "$fmt")\"";
-    
-  # turn off synctex when compiling document fragments.
-  synctex=0; rm -f "$syn";
-  
-  #fragment="$document-frag";
-
-  #document="$TMPDIR/$(basename "$document")";
-  
-  #echo "\\begin{document}" > "$document";
-  #cat "$fragment" >> "$document";
-  #echo >> "$document"; 
-  #echo "\\end{document}" >> "$document"; 
+  # add the fmt file as a command line argument.
+  # This is a lot easier than trying to create a
+  # new file with a  %! firstline.
+  flags="$flags -fmt=\"$(basename -s ".fmt" "$fmt")\"";
   
 fi
 
@@ -86,31 +98,25 @@ fi
 if [[ -a "$ltx" ]]; then
   pushd "$(dirname "$ltx")" > /dev/null
   
-  # do we need to update the fmt file?
+  # do we need to update the fmt file?  (check that the fmt file doesn't exist or the ltx file is newer.)
   if [[ ! -e "$fmt" || "$ltx" -nt "$fmt" ]]; then
     
-    echo "-->Precompiling format file $(basename "$fmt")";
-    
-    if [ -n "$TM_LATEX_DEBUG" ]; then
-      eval echo '$' ${@:1:$#-1} '$args' -jobname='"$(basename -s '.ltx' "$ltx")"' \
-                                -ini '\&latex' '\"$ltx\"' '\\\\dump' >&2;
-    fi
-
+    info "Precompiling format file $(basename "$fmt")";
     
     # compile preamble
-    eval ${@:1:$#-1} '$args' -jobname='"$(basename -s '.ltx' "$ltx")"' \
+    perform_command $engine $flags -jobname='"$(basename -s '.ltx' "$ltx")"' \
                      -ini '\&latex' '\"$ltx\"' '\\dump';
     
     rc=$?;
     if [ $rc -ne 0 ]; then
-      echo "-->Failed to dump the default format file, quitting"
+      info "Failed to dump the format file, quitting"
       exit $rc;
     fi
     
-    #rm "$(dirname "$ltx")/$(basename -s .ltx "$ltx").(log|pdf)";
+    rm "$(dirname "$ltx")/$(basename -s .ltx "$ltx").(log|pdf)";
     
   else
-    echo "-->Using precompiled format $(basename "$fmt")"
+    info "Using precompiled format $(basename "$fmt")"
   fi
     
   popd > /dev/null
@@ -118,7 +124,7 @@ fi
 
 # Trash outdated auxillary files to force TeX to regenerate them.
 if [ -e "$blg" ]; then
-  awk '{ match($0, /^Database file #[0-9]+: /); if (RLENGTH > 0) { print substr($0, RLENGTH+1); } }' < "$blg" | while read bib; do
+  cat "$blg" | awk '{ match($0, /^Database file #[0-9]+: /); if (RLENGTH > 0) { print substr($0, RLENGTH+1); } }' | while read bib; do
     if [ "$bib" -nt "$aux" ]; then
       rm "$idx" "$aux" "$blg" "$bbl" "$pdf";
       break;
@@ -129,32 +135,31 @@ fi
 # some flags to help us determine when to run or not run
 ranbibtex=0; rerun=1;
 
+
+if [ -n "$TM_LATEX_CLEAN_FIRST" ];then
+  info 'Cleaning';
+  seperator;
+  latexmk -CA;
+fi
+
+# this is the main compile loop.
+# we always start by running 
+
 for i in `jot 4`; do # we never need more than five iterations.
   
   # check if we are done compiling.
   if [ $rerun -eq 0 ]; then break; else rerun=0; fi
 
-  if [ $i -gt 1 ]; then
-    echo "--------------------------------------------------------------------------------";
-  fi
+  if [ $i -gt 1 ]; then seperator; fi
   
   # get index/citation hashes so we can notice if they change.
   if [ -e "$idx" ]; then idxhash=$(md5 -q "$idx"); fi
-  if [[ -e "$aux" && $(egrep '^\\bibdata' "$aux") ]]; then
-    bibhash=$(egrep '^\\bib' "$aux" | md5 -q);
-  fi
+  if [ -e "$aux" ]; then bibhash=$(egrep '^\\bib' "$aux" | md5 -q); fi
   
-  echo "-->Typesetting $(basename "$document")";
-  
-  if [ -n "$TM_LATEX_DEBUG" ]; then
-    eval echo '$' "${@:1:$#-1}" $args \
-                                -jobname='"$jobname"' \
-                                -synctex='"$synctex"' \
-                                '"$document"' >&2;
-  fi
+  info "Typesetting $(basename "$document")";
   
   # run latex and watch the output for lines that tell us to run again.
-  eval ${@:1:$#-1} $args \
+  perform_command $engine $flags \
                    -jobname='"$jobname"' \
                    -synctex='"$synctex"' \
                    '"$document"' \
@@ -166,23 +171,24 @@ for i in `jot 4`; do # we never need more than five iterations.
   
   # run makeindex if the idx changed.
   if [[ -e "$idx" && $idxhash != $(md5 -q "$idx") ]]; then
-    echo "--------------------------------------------------------------------------------";
+    seperator;
+    info 'Making index';
     makeindex "$idx";
     rerun=1;
   fi
   
-  # if the aux file was just created and has a \bibdata line or if bibhash has changed then run bibtex.
-  if [[ -z "$ranbib" && \
-        (-z "$bibhash" && $(egrep '^\\bibdata' "$aux") || \
-         -n "$bibhash" && "$bibhash" != $(egrep '^\\bib' "$aux" | md5 -q)) ]];
-  then
-    echo "--------------------------------------------------------------------------------";
-    echo "-->Compiling bibliography";
-    eval bibtex "$TM_BIBTEX_FLAGS" '"$aux"';
+  
+  
+  if [[ -z "$ranbib" && "$bibhash" != $(egrep '^\\bib' "$aux" | md5 -q) ]];
+    then
+    
+    seperator;
+    info 'Compiling bibliography';
+    perform_command bibtex "$TM_BIBTEX_FLAGS" '"$aux"';
+    
     ranbib=1; rerun=1;
   fi
 
 done
 
 mv "$pdf" "$basename.pdf"
-if [ $synctex -eq 1 ]; then mv "$syn" "$basename.synctex.gz"; fi
